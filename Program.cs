b8613a0748dbd16b91a4f94a84648c54a3c2c64a
Puntang Emiliano <Puntang.emiliano@gmail.com>
2022-10-11 21:22:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*Solicite al usuario que ingrese su nombre completo y guárdelo en una variable del tipo
+correspondiente. Muestre por pantalla un saludo personalizado usando el nombre
+almacenado. Se debe esperar que el usuario presione una tecla para finalizar programa*/
+
+using System;
+
+namespace EJ2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ForegroundColor = ConsoleColor.DarkYellow;
+           
+            string  Nombreing;
+            Console.WriteLine(" *****BIENBENIDOOO***** ");
+            Console.WriteLine("Por favor Ingrese su nombre: ");
+            
+            Nombreing= Console.ReadLine();
+            Console.WriteLine ("Hola " + Nombreing);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
